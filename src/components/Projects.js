ecd26791782a965d4c0c8197dcb1783cfa560aa2
mohamed589m <mohamed53634372@gmail.com>
2024-02-19 18:40:20 +0200

    Personal Portfolio Template

---
+++ src/components/Projects.js
@@ -0,0 +1,119 @@
+import { Container, Row, Col, Nav, Tab } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import "animate.css";
+import TrackVisibility from "react-on-screen";
+
+export const Projects = () => {
+  const projects = [
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+  ];
+
+  return (
+    <section className="project" id="project">
+      <Container>
+        <Row>
+          <Col>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__bounce" : ""
+                  }
+                >
+                  <h2>Projects</h2>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisci elit, sed
+                    eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut
+                    enim ad minim veniam, quis nostrum exercitationem ullam
+                    corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
+                    consequatur.
+                  </p>
+                </div>
+              )}
+            </TrackVisibility>
+            <Tab.Container id="projects-tabs" defaultActiveKey="first">
+              <Nav
+                variant="pills"
+                className="nav-pills mb-5 justify-content-center align-items-center"
+                id="pills-tab"
+              >
+                <Nav.Item>
+                  <Nav.Link eventKey="first">Tab One</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="second">Tab Two</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="third">Tab Three</Nav.Link>
+                </Nav.Item>
+              </Nav>
+              <Tab.Content>
+                <Tab.Pane eventKey="first">
+                  <Row>
+                    {projects.map((project, index) => {
+                      return <ProjectCard key={index} {...project} />;
+                    })}
+                  </Row>
+                </Tab.Pane>
+                <Tab.Pane eventKey="second">
+                  Lorem ipsum dolor sit amet, consectetur adipisci elit, sed
+                  eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut
+                  enim ad minim veniam, quis nostrum exercitationem ullam
+                  corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
+                  consequatur.
+                </Tab.Pane>
+                <Tab.Pane eventKey="third">
+                  Lorem ipsum dolor sit amet, consectetur adipisci elit, sed
+                  eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut
+                  enim ad minim veniam, quis nostrum exercitationem ullam
+                  corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
+                  corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
+                  corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
+                  corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
+                  corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
+                  corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
+                  corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
+                  consequatur.
+                </Tab.Pane>
+              </Tab.Content>
+            </Tab.Container>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2} />
+    </section>
+  );
+};
